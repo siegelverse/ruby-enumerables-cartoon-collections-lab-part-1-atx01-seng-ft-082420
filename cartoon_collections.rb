4ff90c4718c482f4d297ec4d6cc9afbd695559a6
Marcus Siegel <msiegel37@gmail.com>
2020-08-11 22:58:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,7 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |index, array|
-    puts "#{array}. #{array}" 
+    num = array.to_i + 1 
+    puts "#{num}. #{index}" 
   end 
 end
